dd9e7ba05a1fa90fdb277a2119ad1e9b2301f1d8
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-11-30 11:27:29 +0300

    implemented enum transferFilter

--- src/app/interfaces/filters.ts
+++ src/app/interfaces/filters.ts
@@ -1,8 +1,9 @@
 export enum priceSort { max = 'max', min = 'min', duration = 'duration' }
+export enum transferFilter { withoutTransfers = 'withoutTransfers', oneTransplant = 'oneTransplant' }
 
 export interface Filters {
     priceSort: priceSort,
-    transferFilter: string,
+    transferFilter: transferFilter,
     minCost: number,
     maxCost: number,
     airlines: string
